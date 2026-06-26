a54da3ceb57509a59f852c406904d9d23a5fcc7e
PoojaManjunath17 <pooja.m.techwork@gmail.com>
2026-06-26 16:04:32 +0530

    Day 4: Implemented Kafka producer using PaySim dataset

--- producer/producer.py
+++ producer/producer.py
@@ -1,18 +1,28 @@
 from kafka import KafkaProducer
+import pandas as pd
 import json
+import time
 
 producer = KafkaProducer(
     bootstrap_servers='localhost:9092',
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
 
-transaction = {
-    "transaction_id": 1,
-    "amount": 5000,
-    "type": "TRANSFER"
-}
+# Read the dataset
+df = pd.read_csv("data/PS_20174392719_1491204439457_log.csv")
 
-producer.send("transactions", transaction)
+print("Sending transactions to Kafka...")
+
+# Send first 100 transactions
+for _, row in df.head(100).iterrows():
+    transaction = row.to_dict()
+
+    producer.send("transactions", transaction)
+
+    print(f"Sent: {transaction['type']} | Amount: {transaction['amount']}")
+
+    time.sleep(1)
+
 producer.flush()
 
-print("Transaction sent successfully!")
+print("All transactions sent successfully!")
