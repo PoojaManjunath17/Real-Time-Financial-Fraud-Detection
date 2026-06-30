27be6ad036d6ef1609631f57a9500c9a652d13c8
PoojaManjunath17 <pooja.m.techwork@gmail.com>
2026-06-30 12:20:34 +0530

    Day 9: Encoded transaction type using LabelEncoder

---
+++ analysis/feature_engineering.py
@@ -0,0 +1,26 @@
+import os
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
+# Project directory
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Dataset path
+csv_path = os.path.join(
+    BASE_DIR,
+    "data",
+    "PS_20174392719_1491204439457_log.csv"
+)
+
+# Read dataset
+df = pd.read_csv(csv_path)
+
+print("Before Encoding:")
+print(df["type"].head())
+
+# Encode transaction type
+encoder = LabelEncoder()
+df["type"] = encoder.fit_transform(df["type"])
+
+print("\nAfter Encoding:")
+print(df["type"].head())
