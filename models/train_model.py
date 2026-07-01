a9342cb286517bd7f62d1cf474847055a4f3a4ca
PoojaManjunath17 <pooja.m.techwork@gmail.com>
2026-07-01 09:54:26 +0530

    Day 10: Added fraud prediction on test dataset

--- models/train_model.py
+++ models/train_model.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+
 # Project directory
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -26,3 +27,19 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 print("\nTraining Data Shape:", X_train.shape)
 print("Testing Data Shape :", X_test.shape)
+
+# Train Logistic Regression Model
+model = LogisticRegression(max_iter=1000)
+
+model.fit(X_train, y_train)
+
+print("\nLogistic Regression Model Trained Successfully!")
+
+# Predict on test data
+y_pred = model.predict(X_test)
+
+print("\nFirst 20 Predictions:")
+print(y_pred[:20])
+
+print("\nFirst 20 Actual Values:")
+print(y_test.values[:20])
