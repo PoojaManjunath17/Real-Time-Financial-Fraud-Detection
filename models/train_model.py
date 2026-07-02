275ff7d4ace437dda3e032b55b251c3380092f1c
PoojaManjunath17 <pooja.m.techwork@gmail.com>
2026-07-02 11:26:37 +0530

    Day 12: Added accuracy evaluation for fraud detection model

--- models/train_model.py
+++ models/train_model.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-
+from sklearn.metrics import accuracy_score
 # Project directory
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -40,6 +40,11 @@ y_pred = model.predict(X_test)
 
 print("\nFirst 20 Predictions:")
 print(y_pred[:20])
+# Accuracy
+accuracy = accuracy_score(y_test, y_pred)
+
+print("\nAccuracy")
+print(f"{accuracy:.4f}")
 
 print("\nFirst 20 Actual Values:")
 print(y_test.values[:20])
