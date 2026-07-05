a514cf81dbe6d7635da3c7c2e6d4d3666d76be61
PoojaManjunath17 <pooja.m.techwork@gmail.com>
2026-07-05 17:52:10 +0530

    Day 17: Added Kafka stream reader using Spark Structured Streaming

--- streaming/spark_stream.py
+++ streaming/spark_stream.py
@@ -1,5 +1,4 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col
 
 spark = SparkSession.builder \
     .appName("FraudDetectionStreaming") \
@@ -8,26 +7,22 @@ spark = SparkSession.builder \
         "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1"
     ) \
     .getOrCreate()
-print("=" * 50)
-print("SPARK STREAMING CONFIGURATION")
-print("=" * 50)
-print("Kafka Server : localhost:9092")
-print("Kafka Topic  : transactions_v2")
-print("Spark Version:", spark.version)
-print("=" * 50)
 
+# Read Kafka Stream
 df = spark.readStream \
     .format("kafka") \
     .option("kafka.bootstrap.servers", "localhost:9092") \
     .option("subscribe", "transactions_v2") \
     .load()
 
+# Convert binary to string
 transactions = df.selectExpr(
     "CAST(key AS STRING)",
     "CAST(value AS STRING)"
 )
 
 print("Kafka Streaming DataFrame Created!")
+
 transactions.printSchema()
 
 spark.stop()
